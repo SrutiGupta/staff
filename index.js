ddbdfd98688ba687c45ac5011bea8ba22ad3af72
Tanmay Joddar <tanmayjoddar17@gmail.com>
2025-08-31 18:22:46 +0530

    Index.js symmetry

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const compression = require('compression');
+require("dotenv").config();
+const express = require("express");
+const compression = require("compression");
 
 const app = express();
 
@@ -9,25 +9,24 @@ app.use(compression());
 app.use(express.json());
 
 // Define Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/attendance', require('./routes/attendance'));
-app.use('/api/royalty', require('./routes/royalty'));
-app.use('/api/inventory', require('./routes/inventory'));
-app.use('/api/gift-card', require('./routes/giftCard'));
-app.use('/api/reporting', require('./routes/reporting'));
-app.use('/api/barcode', require('./routes/barcode'));
-app.use('/api/invoice', require('./routes/invoice'));
-app.use('/api/payment', require('./routes/payment'));
-app.use('/api/prescription', require('./routes/prescription'));
-app.use('/api/product', require('./routes/product'));
-app.use('/api/patient', require('./routes/patient'));
-app.use('/api/customer', require('./routes/customer'));
-
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/attendance", require("./routes/attendance"));
+app.use("/api/royalty", require("./routes/royalty"));
+app.use("/api/inventory", require("./routes/inventory"));
+app.use("/api/gift-card", require("./routes/giftCard"));
+app.use("/api/reporting", require("./routes/reporting"));
+app.use("/api/barcode", require("./routes/barcode"));
+app.use("/api/invoice", require("./routes/invoice"));
+app.use("/api/payment", require("./routes/payment"));
+app.use("/api/prescription", require("./routes/prescription"));
+app.use("/api/product", require("./routes/product"));
+app.use("/api/patient", require("./routes/patient"));
+app.use("/api/customer", require("./routes/customer"));
 
 // Centralized Error Handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send("Something broke!");
 });
 
 const port = process.env.PORT || 3000;
