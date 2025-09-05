5687e43c8ccbe67472c22f4ef9efd762987368e6
Tanmay Joddar <tanmayjoddar17@gmail.com>
2025-09-05 12:44:09 +0530

    server

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(` Server is running on http://localhost:${port}`);
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`🚀 Server is running on port ${port}`);
 });
