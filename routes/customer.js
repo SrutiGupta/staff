8f90a6e798f685402aade88f4064c74869834183
Tanmay Joddar <tanmayjoddar17@gmail.com>
2025-09-21 17:08:17 +0530

    hotspot

--- routes/customer.js
+++ routes/customer.js
@@ -15,13 +15,13 @@ router.post("/", auth, createCustomer);
 // Get all customers
 router.get("/", auth, getAllCustomers);
 
+// GET /api/customer/hotspots - Get a list of top customer address hotspots
+router.get("/hotspots", auth, getAddressHotspots);
+
 // Get a single customer by ID
 router.get("/:id", auth, getCustomer);
 
 // POST /api/customer/invoice - Create an invoice for a new walk-in customer
 router.post("/invoice", auth, createCustomerAndInvoice);
-
-// GET /api/customer/hotspots - Get a list of top customer address hotspots
-router.get("/hotspots", auth, getAddressHotspots);
 
 module.exports = router;
