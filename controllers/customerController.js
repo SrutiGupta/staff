2993aae7d3be35b29b24fddb0f872d611d7c30cf
Tanmay Joddar <tanmayjoddar17@gmail.com>
2025-11-15 16:07:11 +0530

    asdf

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = require("../lib/prisma");
 
 const createCustomerAndInvoice = async (req, res) => {
   const { customer, items, staffId, paidAmount, paymentMethod } = req.body;
