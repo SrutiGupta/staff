3814ee8f8babc2fa1fc6eb7dcd575cb4968b1b57
Tanmay Joddar <tanmayjoddar17@gmail.com>
2025-11-15 15:31:00 +0530

    asdf

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,5 +1,4 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = require("../lib/prisma");
 
 // Process a payment for an invoice
 exports.processPayment = async (req, res) => {
